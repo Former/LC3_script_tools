ff270e53e31fcae788eb739716aa0f8eeb86065d
Alexei <AlexeiBv+github751@narod.ru>
2020-08-17 12:06:49 +0300

    unittest: Fix test. All test passed.

--- test/unit/lc3sim-c/test_lc3sim-c.cpp
+++ test/unit/lc3sim-c/test_lc3sim-c.cpp
@@ -2,6 +2,24 @@
 
 #include "vm.h"
 
+#ifdef LC3_32BIT
+#define swap swap32
+
+static uint32_t swap32(uint32_t val) {
+    return (val << 8 * 3) | ((val << 8) & 0x00FF0000) | ((val >> 8) & 0x0000FF00) | (val >> 8 * 3);
+}
+#else
+#define swap swap16
+
+static uint16_t swap16(uint16_t val) {
+    return (val << 8) | (val >> 8);
+}
+#endif
+
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
+
+#define ARRAY_SWAP(data) do { for (size_t i = 0; i < ARRAY_SIZE(data); ++i) data[i] = swap(data[i]); } while(0)
+
 TEST(LC3SIM_C, Create_Destroy)
 {
     vm_ctx vm = vm_create();
@@ -14,7 +32,7 @@ TEST(LC3SIM_C, LoadOS)
     vm_load_result res = vm_load_os(vm);
     vm_destroy(vm);
     
-    ASSERT_EQ(res, VM_LOAD_SUCCESS);
+    EXPECT_EQ(res, VM_LOAD_SUCCESS);
 }
 
 #ifdef LC3_32BIT
@@ -27,6 +45,8 @@ TEST(LC3SIM_C, LoadDataNopOn0)
 {
     lc3_register_type data[] = {0u, 0u, 0xFF, BIG_DATA};
     
+    ARRAY_SWAP(data);
+
     vm_ctx vm = vm_create();
     vm_load_result res = vm_load_data(vm, (const unsigned char*)data, sizeof(data));
 
@@ -38,22 +58,24 @@ TEST(LC3SIM_C, LoadDataNopOn0)
 
     vm_destroy(vm);
 
-    ASSERT_EQ(res_read[0], VM_SUCCESS);
-    ASSERT_EQ(out[0], 0u);
+    EXPECT_EQ(res_read[0], VM_SUCCESS);
+    EXPECT_EQ(out[0], 0u);
 
-    ASSERT_EQ(res_read[1], VM_SUCCESS);
-    ASSERT_EQ(out[1], 0xFF);
+    EXPECT_EQ(res_read[1], VM_SUCCESS);
+    EXPECT_EQ(out[1], 0xFF);
 
-    ASSERT_EQ(res_read[2], VM_SUCCESS);
-    ASSERT_EQ(out[2], BIG_DATA);
+    EXPECT_EQ(res_read[2], VM_SUCCESS);
+    EXPECT_EQ(out[2], BIG_DATA);
 
-    ASSERT_EQ(res, VM_LOAD_SUCCESS);
+    EXPECT_EQ(res, VM_LOAD_SUCCESS);
 }
 
 TEST(LC3SIM_C, LoadDataNopOn100)
 {
     lc3_register_type data[] = {100u, 0u, 0xFF, BIG_DATA};
     
+    ARRAY_SWAP(data);
+
     vm_ctx vm = vm_create();
     vm_load_result res = vm_load_data(vm, (const unsigned char*)data, sizeof(data));
 
@@ -65,22 +87,24 @@ TEST(LC3SIM_C, LoadDataNopOn100)
 
     vm_destroy(vm);
 
-    ASSERT_EQ(res_read[0], VM_SUCCESS);
-    ASSERT_EQ(out[0], 0u);
+    EXPECT_EQ(res_read[0], VM_SUCCESS);
+    EXPECT_EQ(out[0], 0u);
 
-    ASSERT_EQ(res_read[1], VM_SUCCESS);
-    ASSERT_EQ(out[1], 0xFF);
+    EXPECT_EQ(res_read[1], VM_SUCCESS);
+    EXPECT_EQ(out[1], 0xFF);
 
-    ASSERT_EQ(res_read[2], VM_SUCCESS);
-    ASSERT_EQ(out[2], BIG_DATA);
+    EXPECT_EQ(res_read[2], VM_SUCCESS);
+    EXPECT_EQ(out[2], BIG_DATA);
 
-    ASSERT_EQ(res, VM_LOAD_SUCCESS);
+    EXPECT_EQ(res, VM_LOAD_SUCCESS);
 }
 
 TEST(LC3SIM_C, RunNopOn0)
 {
     lc3_register_type data[] = {0u, 0u, 0xFF, 0x13};
-    
+
+    ARRAY_SWAP(data);
+
     vm_ctx vm = vm_create();
     vm_load_result res = vm_load_data(vm, (const unsigned char*)data, sizeof(data));
 
@@ -88,14 +112,16 @@ TEST(LC3SIM_C, RunNopOn0)
 
     vm_destroy(vm);
 
-    ASSERT_EQ(res, VM_LOAD_SUCCESS);
-    ASSERT_EQ(res_run, VM_OPCODE_NOP);
+    EXPECT_EQ(res, VM_LOAD_SUCCESS);
+    EXPECT_EQ(res_run, VM_OPCODE_NOP);
 }
 
 TEST(LC3SIM_C, RunNopOn100)
 {
     lc3_register_type data[] = {100u, 0u, 0xFF, 0x13};
-    
+
+    ARRAY_SWAP(data);
+
     vm_ctx vm = vm_create();
     vm_load_result res = vm_load_data(vm, (const unsigned char*)data, sizeof(data));
 
@@ -103,6 +129,6 @@ TEST(LC3SIM_C, RunNopOn100)
 
     vm_destroy(vm);
 
-    ASSERT_EQ(res, VM_LOAD_SUCCESS);
-    ASSERT_EQ(res_run, VM_OPCODE_NOP);
+    EXPECT_EQ(res, VM_LOAD_SUCCESS);
+    EXPECT_EQ(res_run, VM_OPCODE_NOP);
 }
