751e09d75f47f93206b48d1883e830fc1773dd58
Alexei <AlexeiBv+github751@narod.ru>
2020-08-18 13:48:44 +0300

    lc3sim_cpp: Processor and other interfaces.

--- lc3sim_cpp/lc3sim.h
+++ lc3sim_cpp/lc3sim.h
@@ -3,64 +3,146 @@
 namespace LC3_Sim
 {
 
-typedef uint16_t RegType;
-typedef char Char;
+typedef char        Char;
+typedef bool        Bool;
+typedef uint16_t    RegType;
+typedef size_t      InstructionIndex;
+typedef RegType     AddressType;
 
 class IInputOutput
 {
 public:
     virtual ~IInputOutput() {}
     
-    virtual char GetChar() const = 0;
+    virtual Char GetChar() const = 0;
 
-    virtual void PutChar() = 0;
+    virtual void PutChar(Char a_Word) = 0;
 };
 
+class IVirtualMemory
+{
+public:
+    enum Result
+    {
+        rSuccess,
+        rErrorAddressOutOfRange,
+        rErrorAccess,
+    };
+
+    virtual ~IVirtualMemory() {}
+    
+    virtual Result Read(RegType* a_Value, AddressType a_Address) const = 0;
+
+    virtual Result Write(RegType a_Value, AddressType a_Address) = 0;
+};
+
+// Регистры процессора.
 struct Registers
 {
+    Registers()
+    {
+        m_Reg = {0};
+    }
+    
     enum RegName
     {
-        RN_REG_0 = 0,
-        RN_REG_1,
-        RN_REG_2,
-        RN_REG_3,
-        RN_REG_4,
-        RN_REG_5,
-        RN_REG_6,
-        RN_REG_7,
-        RN_REG_PC,
-        RN_REG_PSR,
-        RN_COUNT,
+        rnReg_0 = 0,
+        rnReg_1,
+        rnReg_2,
+        rnReg_3,
+        rnReg_4,
+        rnReg_5,
+        rnReg_6,
+        rnReg_7,
+        rnReg_PC,  // Командный регистр
+        rnReg_PSR, // Регистр флагов
+        rnCount,
     };
     
-    RegType m_Reg[RN_COUNT];
+    RegType m_Reg[rnCount];
 };
 
+// Параметры конфигурации процессора.
+struct ProcessorConfig
+{
+    ProcessorConfig(AddressType a_ExceptionHandlerAddress, AddressType a_ExceptionMask)
+        :m_ExceptionHandlerAddress(a_ExceptionHandlerAddress),m_ExceptionMask(a_ExceptionMask)
+    {
+        m_ExceptionCount = 0;
+    }
+    
+    AddressType m_ExceptionHandlerAddress;
+    RegType     m_ExceptionMask;
+    RegType     m_ExceptionCount;
+}
+
+// InstructionExecuter выполняет одну инструкцию
+class InstructionExecuter
+{
+private:
+    struct Exception
+    {
+        enum Type
+        {
+            // Программа завершилась успешно
+            etSuccess          = 0x00,
+            etNop              = 0x01,
+            // Ошибки - необходимо запустить exception handler
+            etNotImplemented   = 0x02,
+            etErrorRead        = 0x04,
+            etErrorWrite       = 0x08,
+        };
+        Type            m_Type;
+        AddressType     m_AccessAddress;
+        AddressType     m_ExecuteAddress;
+
+        Exception()
+        {
+            m_Type = etSuccess;
+            m_AccessAddress = 0;
+            m_ExecuteAddress = 0;
+        }
+        Exception(ExceptionType a_Type, AddressType a_ExecuteAddress, AddressType a_AccessAddress)
+            :m_Type(a_Type), m_AccessAddress(a_ExecuteAddress), m_ExecuteAddress(a_AccessAddress)
+        {
+        }
+    };
+public:
+    InstructionExecuter(Registers* a_Registers, IVirtualMemory* a_VirtualMemory, IInputOutput* a_InputOutput);
+
+    Exception ExecuteOneInstruction();
+
+private:
+    Registers*      m_Registers;
+    IVirtualMemory* m_VirtualMemory;
+    IInputOutput*   m_InputOutput;
+};
+
+// Процессор исполняет серию инструкций, обрабатывает исключения
 class Processor
 {
-public: // Types
-    typedef size_t InstructionIndex;
-    typedef RegType AddresType;
+public:
+    Processor(Registers* a_Registers, IVirtualMemory* a_VirtualMemory, IInputOutput* a_InputOutput, ProcessorConfig* a_ProcessorConfig);
     
-    typedef enum {
-        // Success
-        RC_SUCCESS,
-        RC_NOP,
-        // Errors
-        RC_NOT_IMPLEMENTED,
-        RC_ERROR_ADDRES_OUT_OF_RANGE,
-    } ResultCode;
+    void Run(InstructionIndex* a_ExecutedInsructionsCount, InstructionIndex a_MaxExecuteInsructCount);
 
-public: // Functions
-    Processor(Registers* a_Registers, VirtualMemoryType* a_VirtualMemory, IInputOutput* a_InputOutput);
-    
-    InstructionIndex Run(InstructionIndex a_InsructCount);
+    LoadResult LoadObjFile(const char* a_FileName);
+
+    LoadResult LoadData(const unsigned uint8_t* a_Data, size_t a_DataLen);
+
+    enum LoadResult
+    {
+        lrSuccess,
+        lrFileNotFound,
+        lrFileTooLarge,
+        lrWriteError,
+    };
     
 private:
-    Registers*          m_Registers;
-    VirtualMemoryType*  m_VirtualMemory;
-    IInputOutput*       m_InputOutput;
+    Registers*       m_Registers;
+    IVirtualMemory*  m_VirtualMemory;
+    IInputOutput*    m_InputOutput;
+    ProcessorConfig* m_ProcessorConfig;
 };
-
 
 } // namespace LC3_Sim
