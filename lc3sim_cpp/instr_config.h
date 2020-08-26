087ed57969abb3d212c8387e56eb348bec51b00c
Alexei Bezborodov <AlexeiBv@yandex.ru>
2020-08-26 21:56:07 +0300

    lc3sim_cpp: Add instr_parser. Change instr_config to instr_parser.

--- lc3sim_cpp/instr_config.h
+++ lc3sim_cpp/instr_config.h
@@ -2,69 +2,66 @@
 #pragma once
 
 #include "sim_types.h"
+#include "instr_parser.h"
 
-#define INSTRUCTION_BIT_COUNT       LC3_INSTRUCTION_BIT_COUNT
-#define OPER_CODE_BIT_COUNT         4
-#define REG_NUM_BIT_COUNT           3
+#define INSTRUCTION_BIT_COUNT           LC3_INSTRUCTION_BIT_COUNT
+#define OPER_CODE_BIT_COUNT             4
+#define REG_NUM_BIT_COUNT               3
 
-#define OPCODE_BR                   0b0000
-#define OPCODE_ADD                  0b0001
-#define OPCODE_LD                   0b0010
-#define OPCODE_ST                   0b0011
-#define OPCODE_JSR                  0b0100
-#define OPCODE_AND                  0b0101
-#define OPCODE_LDR                  0b0110
-#define OPCODE_STR                  0b0111
-#define OPCODE_RTI                  0b1000
-#define OPCODE_NOT                  0b1001
-#define OPCODE_LDI                  0b1010
-#define OPCODE_STI                  0b1011
-#define OPCODE_JMP                  0b1100
-#define OPCODE_RES                  0b1101
-#define OPCODE_LEA                  0b1110
-#define OPCODE_TRAP                 0b1111
+#define OPCODE_BR                       0b0000
+#define OPCODE_ADD                      0b0001
+#define OPCODE_LD                       0b0010
+#define OPCODE_ST                       0b0011
+#define OPCODE_JSR                      0b0100
+#define OPCODE_AND                      0b0101
+#define OPCODE_LDR                      0b0110
+#define OPCODE_STR                      0b0111
+#define OPCODE_RTI                      0b1000
+#define OPCODE_NOT                      0b1001
+#define OPCODE_LDI                      0b1010
+#define OPCODE_STI                      0b1011
+#define OPCODE_JMP                      0b1100
+#define OPCODE_RES                      0b1101
+#define OPCODE_LEA                      0b1110
+#define OPCODE_TRAP                     0b1111
 
-#define I1                          1ul
+#define OPER_CODE_MOVE_BIT              LC3_OPER_CODE_MOVE_BIT(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT)
+#define OPER_CODE_MASK                  LC3_OPER_CODE_MASK(OPER_CODE_BIT_COUNT)
+#define REG_NUM_MASK                    LC3_REG_NUM_MASK(REG_NUM_BIT_COUNT)
 
-#define INT_VALUE(instr, bit_count) ((((instr) & ((I1 << (bit_count)) - 1)) ^ (I1 << ((bit_count) - 1))) - (I1 << ((bit_count) - 1)))
+#define OPER_CODE(instr)                LC3_OPER_CODE(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT)
 
-#define OPER_CODE_MOVE_BIT          (INSTRUCTION_BIT_COUNT - OPER_CODE_BIT_COUNT)
-#define OPER_CODE_MASK              ((I1 << OPER_CODE_BIT_COUNT) - 1)
-#define REG_NUM_MASK                ((I1 << REG_NUM_BIT_COUNT) - 1)
+#define INT_AFTER_OPER_MASK             LC3_INT_AFTER_OPER_MASK(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT)
+#define INT_AFTER_OPER_WITH_FLAG_MASK   LC3_INT_AFTER_OPER_WITH_FLAG_MASK(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT)
+#define INT_AFTER_OPER(instr)           LC3_INT_AFTER_OPER(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT)
+#define INT_AFTER_OPER_FLAG_BIT         LC3_INT_AFTER_OPER_FLAG_BIT(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT)
+#define INT_AFTER_OPER_FLAG(instr)      LC3_INT_AFTER_OPER_FLAG(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT)
+#define INT_AFTER_OPER_WITH_FLAG(instr) LC3_INT_AFTER_OPER_WITH_FLAG(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT)
 
-#define OPER_CODE(instr)            ((instr >> OPER_CODE_MOVE_BIT) & OPER_CODE_MASK)
+#define REG_NUM1_MOVE_BIT               LC3_REG_NUM1_MOVE_BIT(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define REG_NUM1_MASK                   LC3_REG_NUM1_MASK(REG_NUM_BIT_COUNT)
+#define REG_NUM1(instr)                 LC3_REG_NUM1(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
 
-#define INT_AFTER_OPER_MASK         ((I1 << OPER_CODE_MOVE_BIT) - 1)
-#define INT_AFTER_OPER_WITH_FLAG_MASK ((I1 << (OPER_CODE_MOVE_BIT - 1)) - 1)
-#define INT_AFTER_OPER(instr)       INT_VALUE(instr, OPER_CODE_MOVE_BIT)
-#define INT_AFTER_OPER_FLAG_BIT     (OPER_CODE_MOVE_BIT - 1)
-#define INT_AFTER_OPER_FLAG(instr)  (instr & (I1 << INT_AFTER_OPER_FLAG_BIT))
-#define INT_AFTER_OPER_WITH_FLAG(instr) INT_VALUE(instr, OPER_CODE_MOVE_BIT - 1)
+#define INT_AFTER_NUM1_MASK             LC3_INT_AFTER_NUM1_MASK(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define INT_AFTER_NUM1_WITH_FLAG_MASK   LC3_INT_AFTER_NUM1_WITH_FLAG_MASK(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define INT_AFTER_NUM1(instr)           LC3_INT_AFTER_NUM1(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define INT_AFTER_NUM1_FLAG_BIT         LC3_INT_AFTER_NUM1_FLAG_BIT(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define INT_AFTER_NUM1_FLAG(instr)      LC3_INT_AFTER_NUM1_FLAG(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define INT_AFTER_NUM1_WITH_FLAG(instr) LC3_INT_AFTER_NUM1_WITH_FLAG(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
 
-#define REG_NUM1_MOVE_BIT           (OPER_CODE_MOVE_BIT - REG_NUM_BIT_COUNT)
-#define REG_NUM1_MASK               REG_NUM_MASK
-#define REG_NUM1(instr)             ((instr >> REG_NUM1_MOVE_BIT) & REG_NUM1_MASK)
+#define REG_NUM2_MOVE_BIT               LC3_REG_NUM2_MOVE_BIT(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define REG_NUM2_MASK                   LC3_REG_NUM2_MASK(REG_NUM_BIT_COUNT)
+#define REG_NUM2(instr)                 LC3_REG_NUM2(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
 
-#define INT_AFTER_NUM1_MASK         ((I1 << REG_NUM1_MOVE_BIT) - 1)
-#define INT_AFTER_NUM1_WITH_FLAG_MASK ((I1 << (REG_NUM1_MOVE_BIT - 1)) - 1)
-#define INT_AFTER_NUM1(instr)       INT_VALUE(instr, REG_NUM1_MOVE_BIT)
-#define INT_AFTER_NUM1_FLAG_BIT     (REG_NUM1_MOVE_BIT - 1)
-#define INT_AFTER_NUM1_FLAG(instr)  (instr & (I1 << INT_AFTER_NUM1_FLAG_BIT))
-#define INT_AFTER_NUM1_WITH_FLAG(instr) INT_VALUE(instr, REG_NUM1_MOVE_BIT - 1)
+#define REG_NUM3_MOVE_BIT               LC3_REG_NUM3_MOVE_BIT
+#define REG_NUM3_MASK                   LC3_REG_NUM3_MASK(REG_NUM_BIT_COUNT)
+#define REG_NUM3(instr)                 LC3_REG_NUM3(instr, REG_NUM_BIT_COUNT)
 
-#define REG_NUM2_MOVE_BIT           (REG_NUM1_MOVE_BIT - REG_NUM_BIT_COUNT)
-#define REG_NUM2_MASK               REG_NUM_MASK
-#define REG_NUM2(instr)             ((instr >> REG_NUM2_MOVE_BIT) & REG_NUM2_MASK)
+#define INT_AFTER_NUM2_MASK             LC3_INT_AFTER_NUM2_MASK(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define INT_AFTER_NUM2_WITH_FLAG_MASK   LC3_INT_AFTER_NUM2_WITH_FLAG_MASK(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define INT_AFTER_NUM2(instr)           LC3_INT_AFTER_NUM2(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define INT_AFTER_NUM2_FLAG_BIT         LC3_INT_AFTER_NUM2_FLAG_BIT(INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define INT_AFTER_NUM2_FLAG(instr)      LC3_INT_AFTER_NUM2_FLAG(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
+#define INT_AFTER_NUM2_WITH_FLAG(instr) LC3_INT_AFTER_NUM2_WITH_FLAG(instr, INSTRUCTION_BIT_COUNT, OPER_CODE_BIT_COUNT, REG_NUM_BIT_COUNT)
 
-#define REG_NUM3_MOVE_BIT           0
-#define REG_NUM3_MASK               REG_NUM_MASK
-#define REG_NUM3(instr)             ((instr >> REG_NUM3_MOVE_BIT) & REG_NUM3_MASK)
-
-#define INT_AFTER_NUM2_MASK         ((I1 << REG_NUM2_MOVE_BIT) - 1)
-#define INT_AFTER_NUM2_WITH_FLAG_MASK ((I1 << (REG_NUM2_MOVE_BIT - 1)) - 1)
-#define INT_AFTER_NUM2(instr)       INT_VALUE(instr, REG_NUM2_MOVE_BIT)
-#define INT_AFTER_NUM2_FLAG_BIT     (REG_NUM2_MOVE_BIT - 1)
-#define INT_AFTER_NUM2_FLAG(instr)  (instr & (I1 << INT_AFTER_NUM2_FLAG_BIT))
-#define INT_AFTER_NUM2_WITH_FLAG(instr)  INT_VALUE(instr, REG_NUM2_MOVE_BIT - 1)
-
-#define TRAP_MASK                   0xFF
+#define TRAP_MASK                       LC3_TRAP_MASK
---
+++ lc3sim_cpp/instr_parser.h
@@ -0,0 +1,79 @@
+// Copyright 2020 by Alexei Bezborodov <AlexeiBv@narod.ru>
+#pragma once
+
+#define I1                      1ul
+
+#define INT_VALUE(instr, bit_count) \
+                                ((((instr) & ((I1 << (bit_count)) - 1)) ^ (I1 << ((bit_count) - 1))) - (I1 << ((bit_count) - 1)))
+
+#define LC3_OPER_CODE_MOVE_BIT(instr_bit_count, oper_bit_count) \
+                                (instr_bit_count - oper_bit_count)
+#define LC3_OPER_CODE_MASK(oper_bit_count) \
+                                ((I1 << oper_bit_count) - 1)
+#define LC3_REG_NUM_MASK(reg_num_bit_count) \
+                                ((I1 << reg_num_bit_count) - 1)
+
+#define LC3_OPER_CODE(instr, instr_bc, op_bc) \
+                                ((instr >> LC3_OPER_CODE_MOVE_BIT(instr_bc, op_bc)) & LC3_OPER_CODE_MASK(instr_bc))
+
+#define LC3_INT_AFTER_OPER_MASK(instr_bc, op_bc) \
+                                ((I1 << LC3_OPER_CODE_MOVE_BIT(instr_bc, op_bc)) - 1)
+#define LC3_INT_AFTER_OPER_WITH_FLAG_MASK(instr_bc, op_bc) \
+                                ((I1 << (LC3_OPER_CODE_MOVE_BIT(instr_bc, op_bc) - 1)) - 1)
+#define LC3_INT_AFTER_OPER(instr, instr_bc, op_bc) \
+                                INT_VALUE(instr, LC3_OPER_CODE_MOVE_BIT(instr_bc, op_bc))
+#define LC3_INT_AFTER_OPER_FLAG_BIT(instr_bc, op_bc) \
+                                (LC3_OPER_CODE_MOVE_BIT(instr_bc, op_bc) - 1)
+#define LC3_INT_AFTER_OPER_FLAG(instr, instr_bc, op_bc) \
+                                (instr & (I1 << LC3_INT_AFTER_OPER_FLAG_BIT(instr_bc, op_bc)))
+#define LC3_INT_AFTER_OPER_WITH_FLAG(instr, instr_bc, op_bc) \
+                                INT_VALUE(instr, LC3_OPER_CODE_MOVE_BIT(instr_bc, op_bc) - 1)
+
+#define LC3_REG_NUM1_MOVE_BIT(instr_bc, op_bc, rn_bc) \
+                                (LC3_OPER_CODE_MOVE_BIT(instr_bc, op_bc) - rn_bc)
+#define LC3_REG_NUM1_MASK(rn_bc) \
+                                LC3_REG_NUM_MASK(rn_bc)
+#define LC3_REG_NUM1(instr, instr_bc, op_bc, rn_bc) \
+                                ((instr >> LC3_REG_NUM1_MOVE_BIT(instr_bc, op_bc, rn_bc)) & LC3_REG_NUM1_MASK(rn_bc))
+
+#define LC3_INT_AFTER_NUM1_MASK(instr_bc, op_bc, rn_bc) \
+                                ((I1 << LC3_REG_NUM1_MOVE_BIT(instr_bc, op_bc, rn_bc)) - 1)
+#define LC3_INT_AFTER_NUM1_WITH_FLAG_MASK(instr_bc, op_bc, rn_bc) \
+                                ((I1 << (LC3_REG_NUM1_MOVE_BIT(instr_bc, op_bc, rn_bc) - 1)) - 1)
+#define LC3_INT_AFTER_NUM1(instr, instr_bc, op_bc, rn_bc) \
+                                INT_VALUE(instr, LC3_REG_NUM1_MOVE_BIT(instr_bc, op_bc, rn_bc))
+#define LC3_INT_AFTER_NUM1_FLAG_BIT(instr_bc, op_bc, rn_bc) \
+                                (LC3_REG_NUM1_MOVE_BIT(instr_bc, op_bc, rn_bc) - 1)
+#define LC3_INT_AFTER_NUM1_FLAG(instr, instr_bc, op_bc, rn_bc) \
+                                (instr & (I1 << LC3_INT_AFTER_NUM1_FLAG_BIT(instr_bc, op_bc, rn_bc)))
+#define LC3_INT_AFTER_NUM1_WITH_FLAG(instr, instr_bc, op_bc, rn_bc) \
+                                INT_VALUE(instr, LC3_REG_NUM1_MOVE_BIT(instr_bc, op_bc, rn_bc) - 1)
+
+#define LC3_REG_NUM2_MOVE_BIT(instr_bc, op_bc, rn_bc) \
+                                (LC3_REG_NUM1_MOVE_BIT(instr_bc, op_bc, rn_bc) - rn_bc)
+#define LC3_REG_NUM2_MASK(rn_bc) \
+                                LC3_REG_NUM_MASK(rn_bc)
+#define LC3_REG_NUM2(instr, instr_bc, op_bc, rn_bc) \
+                                ((instr >> LC3_REG_NUM2_MOVE_BIT(instr_bc, op_bc, rn_bc)) & LC3_REG_NUM2_MASK(rn_bc))
+
+#define LC3_REG_NUM3_MOVE_BIT \
+                                0
+#define LC3_REG_NUM3_MASK(rn_bc) \
+                                LC3_REG_NUM_MASK(rn_bc)
+#define LC3_REG_NUM3(instr, rn_bc) \
+                                ((instr >> LC3_REG_NUM3_MOVE_BIT) & LC3_REG_NUM3_MASK(rn_bc))
+
+#define LC3_INT_AFTER_NUM2_MASK(instr_bc, op_bc, rn_bc) \
+                                ((I1 << LC3_REG_NUM2_MOVE_BIT(instr_bc, op_bc, rn_bc)) - 1)
+#define LC3_INT_AFTER_NUM2_WITH_FLAG_MASK(instr_bc, op_bc, rn_bc) \
+                                ((I1 << (LC3_REG_NUM2_MOVE_BIT(instr_bc, op_bc, rn_bc) - 1)) - 1)
+#define LC3_INT_AFTER_NUM2(instr, instr_bc, op_bc, rn_bc) \
+                                INT_VALUE(instr, LC3_REG_NUM2_MOVE_BIT(instr_bc, op_bc, rn_bc))
+#define LC3_INT_AFTER_NUM2_FLAG_BIT(instr_bc, op_bc, rn_bc) \
+                                (LC3_REG_NUM2_MOVE_BIT(instr_bc, op_bc, rn_bc) - 1)
+#define LC3_INT_AFTER_NUM2_FLAG(instr, instr_bc, op_bc, rn_bc) \
+                                (instr & (I1 << LC3_INT_AFTER_NUM2_FLAG_BIT(instr_bc, op_bc, rn_bc)))
+#define LC3_INT_AFTER_NUM2_WITH_FLAG(instr, instr_bc, op_bc, rn_bc) \
+                                INT_VALUE(instr, LC3_REG_NUM2_MOVE_BIT(instr_bc, op_bc, rn_bc) - 1)
+
+#define LC3_TRAP_MASK           0xFF
